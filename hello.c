37b7a0efe3c2475b9c8c5029648f5a3c7a84c667
MottiKumar Babu <mottikumarbabu@gmail.com>
2024-08-19 18:17:18 -0500

    Modified recheck module to change greeting message

--- hello.c
+++ hello.c
@@ -5,17 +5,17 @@
 #include <linux/proc_fs.h>
 
 static int __init custom_init(void) {
-	printk(KERN_INFO "Hello world!!");
+	printk(KERN_INFO "Hello world Modified!!");
 	return 0;
 }
 static void __exit custom_exit(void) {
-	printk(KERN_INFO "Goodbye World!!");
+	printk(KERN_INFO "Goodbye World Modified!!");
 }
 
 module_init(custom_init);
 module_exit(custom_exit);
 
 MODULE_AUTHOR("MottiKumar Babu");
-MODULE_DESCRIPTION("My very first Hello World kernel module");
+MODULE_DESCRIPTION("My very first patch for Hello World kernel module");
 MODULE_LICENSE("GPL");
 
