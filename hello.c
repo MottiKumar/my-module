25faa87f2d5fe75094e65e80078f5df97a5ca020
MottiKumar Babu <mottikumarbabu@gmail.com>
2024-08-19 19:40:58 -0500

    Modified module to cause panic message

--- hello.c
+++ hello.c
@@ -6,10 +6,11 @@
 
 static char *message = "default message";
 module_param(message, charp, 0000);
-MODULE_PARM_DESC(message, "A string parameter for the Hello World module");
+MODULE_PARM_DESC(message, "A string parameter for the Hello World module With a pinch of panic");
 
 static int __init custom_init(void) {
 	printk(KERN_INFO "Hello world Modified!! Parameter message: %s\n", message);
+	printk(KERN_ALERT "Kernel panic triggered by hello module\n");
 	return 0;
 }
 
@@ -21,5 +22,5 @@ module_init(custom_init);
 module_exit(custom_exit);
 
 MODULE_AUTHOR("MottiKumar Babu");
-MODULE_DESCRIPTION("My very first patch for Hello World kernel module with string parameter");
+MODULE_DESCRIPTION("My very first patch for Hello World kernel module with string parameter with add-on Panic for kernel");
 MODULE_LICENSE("GPL");
